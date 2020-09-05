ba140a16d27fdb992310ebf255fb1dff6e89e4fb
BernardoModerno <bernardo.moderno@gmail.com>
2020-09-04 23:37:29 -0300

    Dominio

---
+++ Models/Saida.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace sonmarket.Models {
+    public class Saida {
+        public int Id { get; set; }
+        public Produto Produto { get; set; }
+        public float ValorDaVenda { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
