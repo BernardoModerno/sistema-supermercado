49369cb5172dc07f6c1a6ae5b7452c67c7d26969
BernardoModerno <bernardo.moderno@gmail.com>
2020-09-07 00:32:59 -0300

    Salvando categoria

--- Controllers/CategoriasController.cs
+++ Controllers/CategoriasController.cs
@@ -1,15 +1,31 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
+using sonmarket.Data;
 using sonmarket.DTO;
+using sonmarket.Models;
 
 namespace sonmarket.Controllers
 {
     public class CategoriasController : Controller
     {
+        private readonly ApplicationDbContext database;
+        public CategoriasController(ApplicationDbContext database){
+            this.database = database;
+        }
+
         [HttpPost]
         public IActionResult Salvar(CategoriaDTO categoriaTemporaria)
         {
-            return Content("Oi");
+            if(ModelState.IsValid){
+                Categoria categoria = new Categoria();
+                categoria.Nome = categoriaTemporaria.Nome;
+                categoria.Status = true;
+                database.Categorias.Add(categoria);
+                database.SaveChanges();
+                return RedirectToAction("Categorias", "Gestao");
+            }else{
+                return View("../Gestao/NovaCategoria");
+            }
         }
     }
 }
