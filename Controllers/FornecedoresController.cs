06c92b332f61bcc8894297acc04703ac5d2b6fce
BernardoModerno <bernardo.moderno@gmail.com>
2020-09-08 11:18:58 -0300

    Salvando fornecedor

--- Controllers/FornecedoresController.cs
+++ Controllers/FornecedoresController.cs
@@ -1,14 +1,37 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
+using sonmarket.Data;
 using sonmarket.DTO;
+using sonmarket.Models;
 
 namespace sonmarket.Controllers
 {
     public class FornecedoresController : Controller
     {
+        private readonly ApplicationDbContext database;
+        public FornecedoresController(ApplicationDbContext database)
+        {
+            this.database = database;
+        }
+
+        [HttpPost]
         public IActionResult Salvar(FornecedorDTO fornecedorTemporario)
         {
-            return Content("oi");
+            if(ModelState.IsValid)
+            {
+                Fornecedor fornecedor = new Fornecedor();
+                fornecedor.Nome = fornecedorTemporario.Nome;
+                fornecedor.Email = fornecedorTemporario.Email;
+                fornecedor.Telefone = fornecedorTemporario.Telefone;
+                fornecedor.Status = true;
+                database.Fornecedores.Add(fornecedor);
+                database.SaveChanges();
+                return RedirectToAction("Fornecedores", "Gestao");
+            }
+            else
+            {
+                return View("../Gestao/NovoFornecedor");
+            }
         }
     }
 }
