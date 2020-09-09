977587d39a7aef3fad56260344686a9b47af48b1
BernardoModerno <bernardo.moderno@gmail.com>
2020-09-09 15:33:51 -0300

    Salvando produto

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -1,15 +1,41 @@
 using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using sonmarket.Data;
 using sonmarket.DTO;
+using sonmarket.Models;
 
 namespace sonmarket.Controllers
 {
     public class ProdutosController : Controller
     {
+        private readonly ApplicationDbContext database;
+        public ProdutosController(ApplicationDbContext database)
+        {
+            this.database = database;
+        }
         [HttpPost]
         public IActionResult Salvar(ProdutoDTO produtoTemporario)
         {
-            return Content("Oi");
+            if(ModelState.IsValid)
+            {
+                Produto produto = new Produto();
+                produto.Nome = produtoTemporario.Nome;
+                produto.Categoria = database.Categorias.First(categoria => categoria.Id == produtoTemporario.CategoriaID);
+                produto.Fornecedor = database.Fornecedores.First(fornecedor => fornecedor.Id == produtoTemporario.FornecedorID);
+                produto.PrecoDeCusto = produtoTemporario.PrecoDeCusto;
+                produto.PrecoDeVenda = produtoTemporario.PrecoDeVenda;
+                produto.Medicao = produtoTemporario.Medicao;
+                produto.Status = true;
+                database.Produtos.Add(produto);
+                database.SaveChanges();
+                return RedirectToAction("Produtos", "Gestao");
+            }else
+            {
+                ViewBag.Categorias = database.Categorias.ToList(); /*Se der errado retorna a lista de fornecedores e categorias para o formulário*/
+                ViewBag.Fornecedores = database.Fornecedores.ToList();
+                return View("../Gestao/NovoProduto");
+            }
         }
     }
 }
