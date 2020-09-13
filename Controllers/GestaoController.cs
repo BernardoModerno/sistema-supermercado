b9d2f45ca32ffabb6225684764ffce636f247f47
BernardoModerno <bernardo.moderno@gmail.com>
2020-09-13 16:30:45 -0300

    Editando produto

--- Controllers/GestaoController.cs
+++ Controllers/GestaoController.cs
@@ -5,66 +5,68 @@ using Microsoft.EntityFrameworkCore;
 using sonmarket.Data;
 using sonmarket.DTO;
 
-namespace sonmarket.Controllers
-{
-    public class GestaoController : Controller
-    {
+namespace sonmarket.Controllers {
+    public class GestaoController : Controller {
         private readonly ApplicationDbContext database;
-        public GestaoController(ApplicationDbContext database)
-        {
+        public GestaoController (ApplicationDbContext database) {
             this.database = database;
         }
-        public IActionResult Index()
-        {
-            return View();
+        public IActionResult Index () {
+            return View ();
         }
-        public IActionResult Categorias()
-        {
-            var categorias = database.Categorias.Where(cat => cat.Status == true).ToList();
-            return View(categorias);
+        public IActionResult Categorias () {
+            var categorias = database.Categorias.Where (cat => cat.Status == true).ToList ();
+            return View (categorias);
         }
-        public IActionResult NovaCategoria()
-        {
-            return View();
+        public IActionResult NovaCategoria () {
+            return View ();
         }
-        public IActionResult EditarCategoria(int id)
-        {
-            var categoria = database.Categorias.First(cat => cat.Id == id);
-            CategoriaDTO categoriaView = new CategoriaDTO();
+        public IActionResult EditarCategoria (int id) {
+            var categoria = database.Categorias.First (cat => cat.Id == id);
+            CategoriaDTO categoriaView = new CategoriaDTO ();
             categoriaView.Id = categoria.Id;
             categoriaView.Nome = categoria.Nome;
-            return View(categoriaView);
+            return View (categoriaView);
         }
-        public IActionResult Fornecedores()
-        {
-            var fornecedores = database.Fornecedores.Where(forne => forne.Status == true).ToList();
-            return View(fornecedores);
+        public IActionResult Fornecedores () {
+            var fornecedores = database.Fornecedores.Where (forne => forne.Status == true).ToList ();
+            return View (fornecedores);
         }
-        public IActionResult NovoFornecedor()
-        {
-            return View();
+        public IActionResult NovoFornecedor () {
+            return View ();
         }
 
-        public IActionResult EditarFornecedor(int id)
-        {
-            var fornecedor = database.Fornecedores.First(forne => forne.Id == id);
-            FornecedorDTO fornecedorView = new FornecedorDTO();
+        public IActionResult EditarFornecedor (int id) {
+            var fornecedor = database.Fornecedores.First (forne => forne.Id == id);
+            FornecedorDTO fornecedorView = new FornecedorDTO ();
             fornecedorView.Id = fornecedor.Id;
             fornecedorView.Nome = fornecedor.Nome;
             fornecedorView.Email = fornecedor.Email;
             fornecedorView.Telefone = fornecedor.Telefone;
-            return View(fornecedorView);
+            return View (fornecedorView);
         }
-        public IActionResult Produtos()
-        {
-            var produtos = database.Produtos.Include(p => p.Categoria).Include(p => p.Fornecedor).ToList();
-            return View(produtos);
+        public IActionResult Produtos () {
+            var produtos = database.Produtos.Include (p => p.Categoria).Include (p => p.Fornecedor).ToList ();
+            return View (produtos);
         }
-        public IActionResult NovoProduto()
-        {
-            ViewBag.Categorias = database.Categorias.ToList();
-            ViewBag.Fornecedores = database.Fornecedores.ToList();
-            return View();
+        public IActionResult NovoProduto () {
+            ViewBag.Categorias = database.Categorias.ToList ();
+            ViewBag.Fornecedores = database.Fornecedores.ToList ();
+            return View ();
+        }
+        public IActionResult EditarProduto (int id) {
+            var produto = database.Produtos.Include (p => p.Categoria).Include (p => p.Fornecedor).First (p => p.Id == id);
+            ProdutoDTO produtoView = new ProdutoDTO ();
+            produtoView.Id = produto.Id;
+            produtoView.Nome = produto.Nome;
+            produtoView.PrecoDeCusto = produto.PrecoDeCusto;
+            produtoView.PrecoDeVenda = produto.PrecoDeVenda;
+            produtoView.CategoriaID = produto.Categoria.Id;
+            produtoView.FornecedorID = produto.Fornecedor.Id;
+            produtoView.Medicao = produto.Medicao;
+            ViewBag.Categorias = database.Categorias.ToList ();
+            ViewBag.Fornecedores = database.Fornecedores.ToList ();
+            return View (produtoView);
         }
     }
 }
--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -5,37 +5,50 @@ using sonmarket.Data;
 using sonmarket.DTO;
 using sonmarket.Models;
 
-namespace sonmarket.Controllers
-{
-    public class ProdutosController : Controller
-    {
+namespace sonmarket.Controllers {
+    public class ProdutosController : Controller {
         private readonly ApplicationDbContext database;
-        public ProdutosController(ApplicationDbContext database)
-        {
+        public ProdutosController (ApplicationDbContext database) {
             this.database = database;
         }
+
         [HttpPost]
-        public IActionResult Salvar(ProdutoDTO produtoTemporario)
-        {
-            if(ModelState.IsValid)
-            {
-                Produto produto = new Produto();
+        public IActionResult Salvar (ProdutoDTO produtoTemporario) {
+            if (ModelState.IsValid) {
+                Produto produto = new Produto ();
                 produto.Nome = produtoTemporario.Nome;
-                produto.Categoria = database.Categorias.First(categoria => categoria.Id == produtoTemporario.CategoriaID);
-                produto.Fornecedor = database.Fornecedores.First(fornecedor => fornecedor.Id == produtoTemporario.FornecedorID);
+                produto.Categoria = database.Categorias.First (categoria => categoria.Id == produtoTemporario.CategoriaID);
+                produto.Fornecedor = database.Fornecedores.First (fornecedor => fornecedor.Id == produtoTemporario.FornecedorID);
                 produto.PrecoDeCusto = produtoTemporario.PrecoDeCusto;
                 produto.PrecoDeVenda = produtoTemporario.PrecoDeVenda;
                 produto.Medicao = produtoTemporario.Medicao;
                 produto.Status = true;
-                database.Produtos.Add(produto);
-                database.SaveChanges();
-                return RedirectToAction("Produtos", "Gestao");
-            }else
-            {
-                ViewBag.Categorias = database.Categorias.ToList(); /*Se der errado retorna a lista de fornecedores e categorias para o formulário*/
-                ViewBag.Fornecedores = database.Fornecedores.ToList();
-                return View("../Gestao/NovoProduto");
+                database.Produtos.Add (produto);
+                database.SaveChanges ();
+                return RedirectToAction ("Produtos", "Gestao");
+            } else {
+                ViewBag.Categorias = database.Categorias.ToList (); /*Se der errado retorna a lista de fornecedores e categorias para o formulário*/
+                ViewBag.Fornecedores = database.Fornecedores.ToList ();
+                return View ("../Gestao/NovoProduto");
             }
         }
+
+        [HttpPost]
+        public IActionResult Atualizar (ProdutoDTO produtoTemporario) {
+            if (ModelState.IsValid) {
+                var produto = database.Produtos.First (prod => prod.Id == produtoTemporario.Id);
+                produto.Nome = produtoTemporario.Nome;
+                produto.Categoria = database.Categorias.First (categoria => categoria.Id == produtoTemporario.CategoriaID);
+                produto.Fornecedor = database.Fornecedores.First (fornecedor => fornecedor.Id == produtoTemporario.FornecedorID);
+                produto.PrecoDeCusto = produtoTemporario.PrecoDeCusto;
+                produto.PrecoDeVenda = produtoTemporario.PrecoDeVenda;
+                produto.Medicao = produtoTemporario.Medicao;
+                database.SaveChanges ();
+                return RedirectToAction ("Produtos", "Gestao");
+            } else {
+                return RedirectToAction ("Produtos", "Gestao");
+            }
+        }
+
     }
 }
