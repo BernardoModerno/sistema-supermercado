2ac03b716c82d07cc3467f90bbbc9dd1c4830f39
BernardoModerno <bernardo.moderno@gmail.com>
2020-09-16 19:23:29 -0300

    Salvando promoção no banco de dados

---
+++ Controllers/PromocoesController.cs
@@ -0,0 +1,38 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using sonmarket.Data;
+using sonmarket.DTO;
+using sonmarket.Models;
+
+namespace sonmarket.Controllers
+{
+    public class PromocoesController : Controller
+    {
+        private readonly ApplicationDbContext database;
+        public PromocoesController(ApplicationDbContext database)
+        {
+            this.database = database;
+        }
+
+        [HttpPost]
+        public IActionResult Salvar(PromocaoDTO promocaoTemporaria)
+        {
+            if(ModelState.IsValid)
+            {
+                Promocao promocao = new Promocao();
+                promocao.Nome = promocaoTemporaria.Nome;
+                promocao.Produto = database.Produtos.First(prod => prod.Id == promocaoTemporaria.ProdutoID);
+                promocao.Porcentagem = promocaoTemporaria.Porcentagem;
+                promocao.Status = true;
+                database.Promocoes.Add(promocao);
+                database.SaveChanges();
+                return RedirectToAction("Promocoes", "Gestao");
+            }else
+            {
+                ViewBag.Produtos = database.Produtos.ToList();
+                return View("../Gestao/NovaPromocao");
+            }
+        }
+        
+    }
+}
