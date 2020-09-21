ece5ee9d7ee1be1515296e5458b414c9be0f2ea4
BernardoModerno <bernardo.moderno@gmail.com>
2020-09-21 19:18:37 -0300

    Carregar dados do produto no formulário

--- wwwroot/js/frente.js
+++ wwwroot/js/frente.js
@@ -1,12 +1,25 @@
+/*declaração de variáveis*/ 
 var enderecoProduto = "https://localhost:5001/Produtos/Produto/"
 var produto;
 
+/*Funções*/
+function preencherFormulario(dadosProduto) {
+    $("#campoNome").val(dadosProduto.nome);
+    $("#campoCategoria").val(dadosProduto.categoria.nome);
+    $("#campoFornecedor").val(dadosProduto.fornecedor.nome);
+    $("#campoPreco").val(dadosProduto.precoDeVenda);
+}
+
+
+/*Ajax*/
 $("#pesquisar").click(function() {
     var codProduto = $("#codProduto").val();
     var enderecoTemporario = enderecoProduto+codProduto;
     $.post(enderecoTemporario, function(dados, status){
         
             produto = dados;
+            preencherFormulario(produto);
+            console.log(produto);
         
     }).fail(function(){
         alert("Produto inválido");
